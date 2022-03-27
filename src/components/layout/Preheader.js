8accefe425804a67bac5aa643b9411bea65f3af2
SergioPenagos <sergiops554@gmail.com>
2022-03-26 21:42:14 -0600

    changed ul styles in header

--- src/components/layout/Preheader.js
+++ src/components/layout/Preheader.js
@@ -1,14 +1,21 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import {
   faGithub,
-  faInstagram,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+
+const ul_style = {
+  display: "block",
+  listStyleType: "disc",
+  marginBlockStart: "1em",
+  marginBlockEnd: "1em",
+  marginInlineStart: "0px",
+  marginInlineEnd: "0px",
+  paddingInlineStart: "40px",
+}
 
 function Preheader() {
   return (
@@ -18,7 +25,7 @@ function Preheader() {
           <Col lg={8}>
           </Col>
           <Col lg={4}>
-            <ul>
+            <ul style={ul_style}>
               <li>
                 <a href="https://github.com/SergioPenagos/NewStatsFE" target={"_blank"}>
                   <FontAwesomeIcon icon={faGithub} />
