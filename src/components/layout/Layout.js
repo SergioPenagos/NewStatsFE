f418f61c3931a9b9524d4f101a3a85e0c3db90b9
SergioPenagos <sergiops554@gmail.com>
2022-03-29 15:24:30 -0600

    Changed page title

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,22 @@
 import Preheader from "./Preheader";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/esm/Container";
 
 function Layout(props) {
   return (
     <>
-      <Preheader />
+      <div className="pre-header">
+        <Container>
+          <Row>
+            <Col lg={8}></Col>
+            <Preheader />
+          </Row>
+        </Container>
+      </div>
       <div>
-        <Container style={{display:"flex", justifyContent:"center"}}>
-            <main>{props.children}</main>
+        <Container style={{ display: "flex", justifyContent: "center" }}>
+          <main>{props.children}</main>
         </Container>
       </div>
     </>
--- src/components/layout/Preheader.js
+++ src/components/layout/Preheader.js
@@ -1,11 +1,6 @@
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faLinkedin,
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const ul_style = {
   display: "block",
@@ -15,32 +10,32 @@ const ul_style = {
   marginInlineStart: "0px",
   marginInlineEnd: "0px",
   paddingInlineStart: "40px",
-}
+};
 
 function Preheader() {
   return (
-    <div className="pre-header">
-      <Container>
-        <Row>
-          <Col lg={8}>
-          </Col>
-          <Col lg={4}>
-            <ul style={ul_style}>
-              <li style={{display:"inline-block"}}>
-                <a href="https://github.com/SergioPenagos/NewStatsFE" target={"_blank"}  rel="noreferrer">
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-              </li>
-              <li style={{display:"inline-block"}}>
-                <a href="https://www.linkedin.com/in/sergio-penagos-057327181/" target={"_blank"}  rel="noreferrer">
-                  <FontAwesomeIcon icon={faLinkedin} />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Col lg={4}>
+      <ul style={ul_style}>
+        <li style={{ display: "inline-block" }}>
+          <a
+            href="https://github.com/SergioPenagos/NewStatsFE"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+        <li style={{ display: "inline-block" }}>
+          <a
+            href="https://www.linkedin.com/in/sergio-penagos-057327181/"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+      </ul>
+    </Col>
   );
 }
 
