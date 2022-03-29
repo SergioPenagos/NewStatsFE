5a733684cb15366a443d5bf35a61d51aa6a64a15
SergioPenagos <sergiops554@gmail.com>
2022-03-28 18:16:18 -0600

    [ADD] Added global styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Layout from "./components/layout/Layout";
 import MainPage from "./pages/mainPage";
 import AboutPage from "./pages/aboutPage";
 import Container from "react-bootstrap/Container";
+import GlobalStyle from "./Styles/fontStyle";
 
 function App() {
   return (
     <Layout>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/about" element={<AboutPage />}></Route>
